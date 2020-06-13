4fee85743be70f0ee51d032f447ad2043e1910c1
Kaloyan Angelov <kaloyan.angelov.tues@gmail.com>
2020-06-13 22:34:49 +0300

    [Update] for today that's it

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,50 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var src = './app/scss/**/*.+(scss|sass)'
+var bs = require('browser-sync');
+
+var src = './app/scss/**/*.+(scss|sass)';
+var htmlSrc = './app/*.html';
+var imgSrc = './app/imgs/*';
+
 
 const styles = () =>
   gulp
     .src(src)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./app/css/'));
+    .pipe(gulp.dest('./build/css/'));
 
-const build = gulp.series(
-  gulp.parallel(styles)
-);
+const imgs = () => 
+  gulp
+    .src(imgSrc)
+    .pipe(gulp.dest('./build/imgs/'));
+
+const html = () =>
+  gulp
+    .src(htmlSrc)
+    .pipe(gulp.dest('./build'));
+
+const refresh = () => {
+  bs.init({
+    server: {
+      baseDir: './build'
+    },
+    notify: false
+  });
+
+  gulp.watch(src, styles).on('change', bs.reload);
+  gulp.watch(htmlSrc, html).on('change', bs.reload);
+  gulp.watch(imgSrc, imgs).on('change', bs.reload);
+}
+
+const build = gulp.series(gulp.parallel(styles, imgs));
+
+const watch = gulp.series(build, refresh);
+
+exports.watch = watch;
+
+exports.styles = styles;
 
 exports.build = build;
+
 exports.default = build;
